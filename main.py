ef4ac9d56f64dbb136a71ed39ae8735c7133499f
Ahmad Mujtaba <ahmadmujtaba0202@gmail.com>
2025-03-21 00:00:16 +0500

    Midlabs_project

---
+++ main.py
@@ -0,0 +1,13 @@
+city = input("Enter a city: ")
+adjective_1 = input("Adjective_1: ")
+noun = input("Noun: ")
+verb_1 = input("Verb_1: ")
+verb_2 = input("Verb_2: ")
+place = input("Place: ")
+famous_person = input("Famous Person: ")
+adjective_2 = input("Adjective_2: ")
+
+story = f"Once upon a time in {city}, a {adjective_1} {noun} was {verb_1} near {place}. Suddenly, {famous_person} appeared and changed everything! \n" \
+        f"In the {adjective_2} streets of {city}, a {noun} decided to {verb_2} all the way to {place}. It was truly a day to remember, thanks to {famous_person}!\n" \
+        f"One day, {noun} found a magical portal in {place}. It led to {city}, where {famous_person} was waiting with a surprise!"
+print(story)
